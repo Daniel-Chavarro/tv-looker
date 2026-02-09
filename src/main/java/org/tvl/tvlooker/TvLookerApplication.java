a39049b7437d11d593cd55f485d49787f8c3bb0c
Daniel Alonso Chavarro Chipatecua <108764157+Daniel-Chavarro@users.noreply.github.com>
2026-02-09 18:21:38 -0500

    Created: Created main files (#1)

---
+++ src/main/java/org/tvl/tvlooker/TvLookerApplication.java
@@ -0,0 +1,13 @@
+package org.tvl.tvlooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvLookerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TvLookerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tvl/tvlooker/TvLookerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tvl.tvlooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvLookerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
