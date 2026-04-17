70a7d1ca674cb0dc871152f3be7f9de3b4aebfe3
Daniel Alonso Chavarro Chipatecua <108764157+Daniel-Chavarro@users.noreply.github.com>
2026-04-17 18:18:58 -0500

    Update UserMapper.java

--- src/main/java/org/tvl/tvlooker/api/dto/mapper/UserMapper.java
+++ src/main/java/org/tvl/tvlooker/api/dto/mapper/UserMapper.java
@@ -45,7 +45,7 @@ public class UserMapper {
 
     public static User fromUpdateRequest(UpdateUserRequest request) {
         return User.builder()
-                .username(request.getUsername())
+                .username(request.getName())
                 .password(request.getPassword())
                 .email(request.getEmail())
                 .name(request.getName())
