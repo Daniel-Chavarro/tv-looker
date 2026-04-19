0690c9f0df24f1bf6c7b44f69f8b47d1fec0f865
Daniel Alonso Chavarro Chipatecua <108764157+Daniel-Chavarro@users.noreply.github.com>
2026-04-18 20:08:00 -0500

    feat: add pagination to DirectorController getAllDirectors

--- src/main/java/org/tvl/tvlooker/api/controller/DirectorController.java
+++ src/main/java/org/tvl/tvlooker/api/controller/DirectorController.java
@@ -1,17 +1,21 @@
 package org.tvl.tvlooker.api.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.tvl.tvlooker.api.dto.mapper.DirectorMapper;
+import org.tvl.tvlooker.api.dto.mapper.PageMapper;
 import org.tvl.tvlooker.api.dto.response.DirectorResponse;
+import org.tvl.tvlooker.api.dto.response.PageResponse;
 import org.tvl.tvlooker.domain.model.dto.Director;
 import org.tvl.tvlooker.service.DirectorService;
-
-import java.util.List;
 
 /**
  * REST controller for managing directors.
@@ -23,16 +27,16 @@ public class DirectorController {
     private final DirectorService directorService;
 
     /**
-     * Retrieves all directors.
-     * @return a list of director responses
+     * Retrieves all directors with pagination.
+     * @param pageable Pagination configuration.
+     * @return a paginated list of director responses
      */
     @GetMapping
-    public ResponseEntity<List<DirectorResponse>> getAllDirectors() {
-        List<Director> directors = directorService.getAll();
-        return ResponseEntity.ok(directors.stream()
-                .map(DirectorMapper::toResponse)
-                .toList()
-        );
+    public ResponseEntity<PageResponse<DirectorResponse>> getAllDirectors(
+            @PageableDefault(size = 50, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<Director> directorsPage = directorService.getAll(pageable);
+        PageResponse<DirectorResponse> response = PageMapper.toResponse(directorsPage.map(DirectorMapper::toResponse));
+        return ResponseEntity.ok(response);
     }
 
     /**
