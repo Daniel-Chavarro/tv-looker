bdeb156235ea8af6786b86a82f4ae2da953965d5
Daniel Alonso Chavarro Chipatecua <108764157+Daniel-Chavarro@users.noreply.github.com>
2026-04-20 09:31:05 -0500

    modified put endpoints to patch endpoints

--- src/main/java/org/tvl/tvlooker/api/controller/InteractionController.java
+++ src/main/java/org/tvl/tvlooker/api/controller/InteractionController.java
@@ -9,9 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -92,7 +92,7 @@ public class InteractionController {
      * @param request the request containing updated interaction details
      * @return the updated interaction response
      */
-    @PutMapping("/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<InteractionResponse> updateInteraction(
             @PathVariable Long id,
             @RequestBody UpdateInteractionRequest request) {
